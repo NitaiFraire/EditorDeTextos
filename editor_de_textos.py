a12322817a4322dc3b79cff276eede0b0280b364
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-12-02 20:30:38 -0600

    funcion 'guardar' y 'guardar_como', ajustes de colores

--- editor_de_textos.py
+++ editor_de_textos.py
@@ -3,8 +3,24 @@ from tkinter import filedialog as Filedialog
 from io import open
 
 
+##### FUNCIONES ######## 
+#########################
+
 ruta = "" # Almacena la ruta de un archivo
 
+##### Escribir archivo
+#########################
+def escribir_archivo(ruta):
+
+    contenido = texto.get(1.0, "end-1c") # evitar el salto de linea que se genera al final
+    archivo = open(ruta, 'w+')
+    archivo.write(contenido)
+    archivo.close()
+    mensaje.set("Archivo guardado correctamente")
+
+
+##### Nuevo archivo
+#########################
 def nuevo():
 
     mensaje.set("Nuevo archivo")
@@ -14,6 +30,8 @@ def nuevo():
     root.title("EditorDeTextosBy:Nitai")
 
 
+##### Abrir archivo
+#########################
 def abrir():
 
     mensaje.set("Abrir archivo")
@@ -32,12 +50,33 @@ def abrir():
         root.title(ruta + " - EditorDeTextosBy:Nitai" )
 
 
+##### Guardar
+########################
 def guardar():
+
     mensaje.set("Guardar")
+     
+    if ruta != '':
+        escribir_archivo(ruta)
+    else:
+        guardar_como()
 
 
+##### Guardar como
+########################
 def guardar_como():
+
     mensaje.set("Guardar como")
+    global ruta
+    print(ruta)
+    archivo = Filedialog.asksaveasfile(title="Guardar como", defaultextension=".txt")
+
+    if archivo is not None:
+        ruta = archivo.name
+        escribir_archivo(ruta)
+    else:
+        mensaje.set("Guardado canceldo")
+        ruta = ""
 
 
 
@@ -58,17 +97,19 @@ filemenu.add_separator()
 filemenu.add_command(label="Salir", command=root.quit)
 
 menubar.add_cascade(menu=filemenu, label="Archivo")
+menubar.config(bg="#182333", fg="#42a5f5")
+filemenu.config(bg="#182333", fg="#42a5f5")
 
 
 ##### Caja de texto central
 ##############################
 texto = Text(root)
 texto.pack(fill="both", expand=1)
-texto.config(bd=0, padx=6, pady=4, font=('helvetica', 12))
+texto.config(bd=0, padx=6, pady=4, font=('helvetica', 12), bg="#1b2738", fg="#a3a5a7")
 
 
 ##### Monitor inferior
-##############################
+############################## selectbackground="red"
 mensaje = StringVar()
 mensaje.set("Comienza a editar")
 monitor = Label(root, textvar=mensaje, justify="left")
